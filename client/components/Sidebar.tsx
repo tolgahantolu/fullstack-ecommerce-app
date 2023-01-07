a93b19ecb09b20499c04f684d1f9d12d91df4a81
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-01-07 18:27:26 +0300

    sidebar style fix

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -4,8 +4,39 @@ import { BsTruck, BsGear } from "react-icons/bs";
 import { BiCategory } from "react-icons/bi";
 import { AiOutlineHome } from "react-icons/ai";
 import { IoLocationOutline } from "react-icons/io5";
+import { Key, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const MENU_LIST = [
+    {
+      name: "home",
+      path: "/",
+      icon: <AiOutlineHome />,
+    },
+    {
+      name: "category",
+      path: "/category",
+      icon: <BiCategory />,
+    },
+    {
+      name: "map",
+      path: "/map",
+      icon: <IoLocationOutline />,
+    },
+    {
+      name: "delivery",
+      path: "/delivery",
+      icon: <BsTruck />,
+    },
+    {
+      name: "settings",
+      path: "/settings",
+      icon: <BsGear />,
+    },
+  ];
+  const pathname = useRouter().pathname;
+
   return (
     <section className="w-[200px] min-w-[200px] h-full flex flex-col justify-start items-center gap-y-10 pb-10">
       <div className="w-full flex flex-col items-center gap-y-1 justify-center">
@@ -21,7 +52,30 @@ const Sidebar = () => {
 
       {/* sidebar list */}
       <ul className="text-white flex flex-col gap-y-6">
-        <li>
+        {MENU_LIST.map(
+          (menu: { name: String; path: String | any; icon: any }, i: Key) => (
+            <li key={i}>
+              <Link
+                href={menu.path}
+                className={`flex items-center gap-x-8 text-base text-white pr-6 rounded-full ${
+                  pathname === menu.path ? "bg-theme-light-orange" : ""
+                }`}
+              >
+                <span
+                  className={`p-4 rounded-full ${
+                    pathname === menu.path
+                      ? "bg-theme-dark-orange"
+                      : "bg-theme-dark-grey"
+                  }`}
+                >
+                  {menu.icon}
+                </span>
+                <p className="capitalize">{menu.name}</p>
+              </Link>
+            </li>
+          )
+        )}
+        {/*<li>
           <Link
             href="/"
             className="flex items-center gap-x-8 text-base text-white pr-6 rounded-full bg-theme-light-orange"
@@ -76,7 +130,7 @@ const Sidebar = () => {
             </span>
             <p>Setting</p>
           </Link>
-        </li>
+        </li>*/}
       </ul>
 
       <Image
